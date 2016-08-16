38d0fa9f63880ca5d18a26861de60b6e904f285c
vadim1825 <vadim1825@yandex.ua>
2016-08-17 00:38:29 +0300

    js_homework_1

--- Ввод имени/js/script.js
+++ Ввод имени/js/script.js
@@ -8,13 +8,14 @@ for (var i = 0; i < 5; i++) {
 //Записывает имя пользователя в переменную name
 
 var name = prompt('Введите Ваше имя');
+var value = 'Ошибка!'
 
 //Проверяет наличие имени пользователя в массиве
 
 for (var i = 0; i < names.length; i++) {
-    if (names[i] === name) { 
-	    alert(name + ' вы успешно вошли');
-	}   else {
-          alert('Ошибка!');
-	    }
+    if (names[i] === name) {
+        value = name + ' вы успешно вошли!';
+    }    
 }
+
+alert(value);
